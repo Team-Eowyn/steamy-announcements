b0e27c6994c179a574121557a82757c2c774e52f
Yuri Kim <yurikim123@gmail.com>
2020-05-27 18:06:58 -1000

    updated data generator

--- announcements/client/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable import/extensions */
-/* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-
-ReactDOM.render(<App />, document.getElementById('App'));
